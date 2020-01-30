9ee9912e15414740eaecc45c1a88c0b18b498a3b
Romena Murgor <romenamurgor@gmail.com>
2020-01-30 01:30:45 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,10 +1,8 @@
 describe '#square_array' do
 
-  it 'does not call on collect/map or inject' do
+  it .collect do
     numbers = [1,2,3]
-    expect(numbers).to_not receive(:collect)
-    expect(numbers).to_not receive(:map)
-    expect(numbers).to_not receive(:inject)
+    
     square_array(numbers)
   end
 
